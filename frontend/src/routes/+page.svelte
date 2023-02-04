7bffb7fe447735483e48f4f8951aeadd14ba4802
user <nope@gmail.com>
2023-02-04 13:59:10 +0100

    Used Svelte layout.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import  Message  from "./Message.svelte"
     import ResetButton from "./ResetButton.svelte";
+    import LayoutGrid, { Cell } from "@smui/layout-grid";
 
     let socket;
     let data, message_data;
@@ -15,11 +16,24 @@
              message_data = data.Message;
         })
     });
-
-
 </script>
 
 <h1>Un espion xml basique.</h1>
 
-<Message {data} {message_data} />
-<ResetButton />
+<LayoutGrid>
+        <Cell order=1>
+            <div class="demo-cell"><ResetButton /></div>
+        </Cell>
+        <Cell order=0>
+            <div class="demo-cell"><Message {data} {message_data} /></div>
+        </Cell>
+</LayoutGrid>
+
+<style>
+    .demo-cell {
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
