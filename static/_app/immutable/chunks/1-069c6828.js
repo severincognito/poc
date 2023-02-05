d50a9809c565ce89b5524d228c69d9d2abe2c8d1
user <nope@gmail.com>
2023-02-05 16:49:16 +0100

    Built svelte again.

---
+++ static/_app/immutable/chunks/1-069c6828.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-d1bff40d.js";export{t as component};
---
+++ static/_app/immutable/chunks/2-ec7df101.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-85462b40.js";export{t as component};
